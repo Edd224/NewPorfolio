c4bf51cdd38b14558fba2a13999c72f2de16de1b
Edd224 <enno225@gmail.com>
2024-05-31 12:24:47 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
       plugins: [
         // ...
         gzipPlugin({
-          // Wähle die Dateitypen aus, die du komprimieren möchtest
+        
           filter: /\.(js|css|html|json|svg)$/,
-          // Gzip-Komprimierungseinstellungen (optional)
-          minSize: 1024, // Mindestgröße der komprimierten Datei in Bytes
+    
+          minSize: 1024, 
         }),
       ],
     },
